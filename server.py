7eca296d0f8e1d9190951a3a8e1a64d611e95940
DivyaBabu123 <divyaoct94@gmail.com>
2026-03-02 13:01:48 -0500

    error handling and pylint updates

--- server.py
+++ server.py
@@ -9,12 +9,24 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_anlayzer():
+    '''
+    emotion_analyzer function
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    if response['dominant_emotion'] is None:
+        return " Invalid text! Please try again!."
+    return f"For the given statement, the system response is \
+    'anger': {response['anger']}, 'disgust': {response['disgust']}, \
+    'fear': {response['fear']}, 'joy': {response['joy']} and \
+    'sadness': {response['sadness']}. \
+    The dominant emotion is {response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
+    '''
+    rendering index.html
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
